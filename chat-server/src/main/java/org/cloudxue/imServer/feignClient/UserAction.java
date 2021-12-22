c2b938f8dc08077ca25d60d0210dd12141eacee9
xuexiao <xuexiao@belink.com>
2021-12-22 10:39:28 +0800

    添加feign依赖

--- chat-server/src/main/java/org/cloudxue/imServer/feignClient/UserAction.java
+++ chat-server/src/main/java/org/cloudxue/imServer/feignClient/UserAction.java
@@ -1,5 +1,8 @@
 package org.cloudxue.imServer.feignClient;
 
+import feign.Param;
+import feign.RequestLine;
+
 /**
  * @ClassName UserAction
  * @Description 请描述类的业务用途
@@ -7,6 +10,11 @@ package org.cloudxue.imServer.feignClient;
  * @Date 2021/12/21 下午5:29
  * @Version 1.0
  **/
-public class UserAction {
+public interface UserAction {
+    @RequestLine("GET /login/{username}/{password}")
+    public String loginAction(@Param("username") String username,
+                              @Param("password") String password);
 
+    @RequestLine("GET /{userId}")
+    public String getById(@Param("userId") Integer userId);
 }
